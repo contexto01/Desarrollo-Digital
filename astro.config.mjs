b1a0c92558cc7173deda43c527b8fb72541717a1
Daniel Barrera <daniel.barrera02121@hotmail.com>
2024-11-12 11:40:55 -0500

    Add pricing and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  site: 'https://desarrollodigital.netlify.app/',
+  integrations: [tailwind(), sitemap()]
+})
